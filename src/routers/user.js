6f7959a715a2dcf90db699bd632cbc2caa04f80b
MarcosDanielTorres <marcoss.nqn@gmail.com>
2020-08-11 11:34:44 -0300

    Agregué ruta getUserByID y limpié el controller

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,7 +2,7 @@ import express from 'express';
 import userController from '../routersControllers/userController';
 const router = new express.Router();
 
-router.route('/').get(userController.getUser);
-router.route('/').post(userController.postUser);
+router.route('/').get(userController.getUser).post(userController.postUser);
+router.route('/:id').get(userController.getUserByID);
 
 export default router;
--- src/routersControllers/userController.js
+++ src/routersControllers/userController.js
@@ -4,6 +4,16 @@ const getUser = (req, res) => {
   res.send('Estoy en home babyyy of users ofc');
 };
 
+// for debugging
+const getUserByID = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(400).send(user);
+  } catch (e) {
+    res.status(404).send({ error: 'User not found.' });
+  }
+};
+
 const postUser = async (req, res) => {
   const user = new User(req.body);
   try {
@@ -17,6 +27,6 @@ const postUser = async (req, res) => {
 
 const deleteUser = async (req, res) => {};
 
-const updateuser = async (req, res) => {};
+const updateUser = async (req, res) => {};
 
-export default { getUser, postUser };
+export default { getUser, postUser, deleteUser, updateUser, getUserByID };
