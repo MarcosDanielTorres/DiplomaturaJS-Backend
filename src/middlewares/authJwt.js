c3883f6d59b475c3fa5bd804b5ab2f530a4fec8a
Cristian <borgesvive@hotmail.com>
2020-09-03 11:46:38 -0300

    Remove console logs

--- src/middlewares/authJwt.js
+++ src/middlewares/authJwt.js
@@ -5,10 +5,8 @@ import User from '../models/user';
 export default async function authJWT(req, res, next) {
   try {
     const token = req.header('x-access-token');
-    console.log(token);
-
     const decoded = jwt.verify(token, config.secret);
-    console.log(decoded);
+
     const user = await User.findOne({
       _id: decoded._id,
       'tokens.token': token,
